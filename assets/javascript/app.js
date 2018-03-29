25103684cbde9d8e0f93a764a4256342a226e2cc
Hillary Lavin <hillarylavin@hillarys-mbp.local.valenciacc.edu>
2018-03-29 10:36:23 -0400

    Add app.js file, paste in Intialize Firebase function

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDfwy5wA_H-TJ-nNXsKier9XkTjg60EUcU",
+    authDomain: "train-time-38b6a.firebaseapp.com",
+    databaseURL: "https://train-time-38b6a.firebaseio.com",
+    projectId: "train-time-38b6a",
+    storageBucket: "train-time-38b6a.appspot.com",
+    messagingSenderId: "564655145297"
+};
+firebase.initializeApp(config);
